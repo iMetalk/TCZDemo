59fee2d1b1153f7150410e0633cc86ca1aef9d6f
wangsuyan <wsyxyxs@126.com>
2017-08-29 10:56:50 +0800

    d

---
+++ SwiftStudyDemo/SwiftDemo/Block.swift
@@ -0,0 +1,17 @@
+//
+//  Block.swift
+//  SwiftDemo
+//
+//  Created by WangSuyan on 2017/8/8.
+//  Copyright © 2017年 WangSuyan. All rights reserved.
+//
+
+import Foundation
+
+class Block {
+    func backword(s1: String, s2: String) -> Bool {
+        return s1 > s2
+    }
+    
+    Array;
+}
